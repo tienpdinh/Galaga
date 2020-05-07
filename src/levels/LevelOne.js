2867c68c1746f0f09426f6c09c698a785c9c1910
Tien Dinh <tiendinhphuc@gmail.com>
2020-05-07 18:07:47 -0500

    basic movement control

--- src/levels/LevelOne.js
+++ src/levels/LevelOne.js
@@ -9,16 +9,53 @@ import { Vector } from 'simple-physics-engine';
  * @extends {AbstractLevel}
  */
 export default class LevelOne extends AbstractLevel {
-  cube;
+  enemies;
+  player;
 
   constructor(engine, renderer, camera, switchLevel) {
     super(engine, renderer, camera, switchLevel);
   }
 
   init = () => {
-    // Cube
-    this.cube = new Cube(new Vector());
-    this.engine.addObject(this.cube);
+    this.spawnPlayer(new Vector(0, -70, 400));
+    this.addEventListeners();
+    this.spawnEnemies();
+  };
+
+  spawnPlayer = (pos) => {
+    // The player will be initialized to the bottom middle of the screen
+    this.player = new Cube(pos);
+    this.engine.addObject(this.player);
+  };
+
+  spawnEnemies = () => {};
+
+  movePlayer = (e) => {
+    if (e.keyCode === 68) {
+      // move right
+      this.player.setVel(new Vector(0.1, 0, 0));
+    }
+    if (e.keyCode === 65) {
+      // move left
+      this.player.setVel(new Vector(-0.1, 0, 0));
+    }
+    if (e.keyCode === 87) {
+      // move up
+      this.player.setVel(new Vector(0, 0, -0.1));
+    }
+    if (e.keyCode === 83) {
+      // move down
+      this.player.setVel(new Vector(0, 0, 0.1));
+    }
+  };
+
+  stopPlayer = (e) => {
+    this.player.setVel(new Vector());
+  };
+
+  addEventListeners = () => {
+    window.addEventListener('keydown', this.movePlayer, false);
+    window.addEventListener('keyup', this.stopPlayer, false);
   };
 
   cleanup = () => {};
